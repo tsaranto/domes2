1e7c59e651f64949e26da879b4761321cf46a6ad
Eleni Antoniadi Tsarampoulidi <119889560+tsaranto@users.noreply.github.com>
2025-01-25 03:24:26 +0200

    Add files via upload

--- Greedy.java
+++ Greedy.java
@@ -7,57 +7,62 @@ public class Greedy {
         Reader.Data data = reader.readInput();
         List<Job> jobs = data.getJobs();
         int numProcessors = data.getNumProcessors();
+        greedy(jobs, numProcessors);
+ 
 
+    }
+
+    private static void greedy(List<Job> jobs, int numProcessors) {
         MaxPQ maxPQ = new MaxPQ();
-//arxiko assignment twn diergasiwn sta processors
-        for (int i = 0; i < numProcessors; i++) {
-            Processor processor = new Processor(i+1);
-            try {
-                processor.addJob(jobs.removeFromFront());
-            } catch (EmptyListException e) {
-                System.out.println("No more jobs to assign: " + e.getMessage());
-                break;
-            }
-            maxPQ.insert(processor);
-        }
-//assignment ton diergasion sto processsor me ton mikrotero xrono
-        while (!jobs.isEmpty()) {
-            Job job;
-            try {
-                job = jobs.removeFromFront();
-            } catch (EmptyListException e) {
-                System.out.println("No more jobs to assign: " + e.getMessage());
-                break;
-            }
-            Processor processor = maxPQ.getmax();
-            processor.addJob(job);
-            maxPQ.insert(processor);
-        }
-//ektiposi twn diergasion kai tou sunolikou xronou      
-        int makespan = 0;
-        Processor[] processors = new Processor[numProcessors];
-        for (int i = 0; i < numProcessors; i++) {
-            processors[i] = maxPQ.getmax();
-            if (processors[i] != null) {
-                makespan = Math.max(makespan, processors[i].getTotalProcessingTime());
-            }
-        }
-
-        if (jobs.size() < 50) {
-            for (Processor processor : processors) {
-                if (processor != null) {
-                    System.out.print("id " + processor.getId() + ", load=" + processor.getTotalProcessingTime() + ": ");
-                    List<Job> processedJobs = processor.getProcessedJobs();
-                    for (int i = 0; i < processedJobs.size(); i++) {
-                        System.out.print(processedJobs.get(i).getTime() + " ");
+        //arxiko assignment twn diergasiwn sta processors
+                for (int i = 0; i < numProcessors; i++) {
+                    Processor processor = new Processor(i+1);
+                    try {
+                        processor.addJob(jobs.removeFromFront());
+                    } catch (EmptyListException e) {
+                        System.out.println("No more jobs to assign: " + e.getMessage());
+                        break;
                     }
-                    System.out.println();
+                    maxPQ.insert(processor);
                 }
+        //assignment ton diergasion sto processsor me ton mikrotero xrono
+                while (!jobs.isEmpty()) {
+                    Job job;
+                    try {
+                        job = jobs.removeFromFront();
+                    } catch (EmptyListException e) {
+                        System.out.println("No more jobs to assign: " + e.getMessage());
+                        break;
+                    }
+                    Processor processor = maxPQ.getmax();
+                    processor.addJob(job);
+                    maxPQ.insert(processor);
+                }
+        //ektiposi twn diergasion kai tou sunolikou xronou      
+                int makespan = 0;
+                Processor[] processors = new Processor[numProcessors];
+                for (int i = 0; i < numProcessors; i++) {
+                    processors[i] = maxPQ.getmax();
+                    if (processors[i] != null) {
+                        makespan = Math.max(makespan, processors[i].getTotalProcessingTime());
+                    }
+                }
+        
+                if (jobs.size() < 50) {
+                    for (Processor processor : processors) {
+                        if (processor != null) {
+                            System.out.print("id " + processor.getId() + ", load=" + processor.getTotalProcessingTime() + ": ");
+                            List<Job> processedJobs = processor.getProcessedJobs();
+                            for (int i = 0; i < processedJobs.size(); i++) {
+                                System.out.print(processedJobs.get(i).getTime() + " ");
+                            }
+                            System.out.println();
+                        }
+                    }
+                }
+        
+        
+                System.out.println("Makespan = " + makespan);
             }
-        }
-
-
-        System.out.println("Makespan = " + makespan);
     }
 
-    }

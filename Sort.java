254980ce086cb1e311e7bc37727c078165d2c983
Eleni Antoniadi Tsarampoulidi <119889560+tsaranto@users.noreply.github.com>
2025-01-29 18:53:46 +0200

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -16,21 +16,21 @@ public class Sort {
     }
 
     private static void heapify(List<Job> jobs, int n, int i) {
-        int largest = i; //arxikopoihsh
+        int smallest = i; //arxikopoihsh
         int left = 2 * i + 1; //aristero paidi
         int right = 2 * i + 2; //deksi paidi
-        //an to aristero paidi einai megalutero apo to largest
-        if (left < n && jobs.get(left).getTime() > jobs.get(largest).getTime()) {
-            largest = left;
+        //an to aristero paidi einai mikrotero apo to smallest
+        if (left < n && jobs.get(left).getTime() < jobs.get(smallest).getTime()) {
+            smallest = left;
         }
-        //an to deksi paidi einai megalutero apo to largest
-        if (right < n && jobs.get(right).getTime() > jobs.get(largest).getTime()) {
-            largest = right;
+        //an to deksi paidi einai mikrotero apo to smallest
+        if (right < n && jobs.get(right).getTime() < jobs.get(smallest).getTime()) {
+            smallest = right;
         }
-        //an to largest den einai to i
-        if (largest != i) {
-            swap(jobs, i, largest);
-            heapify(jobs, n, largest);
+        //an to smallest den einai to i
+        if (smallest != i) {
+            swap(jobs, i, smallest);
+            heapify(jobs, n, smallest);
         }
     }
 
